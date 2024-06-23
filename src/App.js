e62a9f6815063c8013e435ca9d596573f3875fe4
AviyaAviya <aviya@kho.org.il>
2024-06-23 22:21:59 +0300

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import TriviaChannel from './TriviaChannel/TriviaChannel';
+import './styles/Home.css';
+
+function Home() {
+    return (
+        <div className="home-container">
+            <h1 className="title">Welcome to the Home Page!</h1>
+            <Link className="cta-button" to="/quiz">Go to Quiz App</Link>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+               
+                
+                <Routes>
+                <Route path="/" element={<Home />} /> 
+                <Route path="/quiz" element={<TriviaChannel />} />
+                
+                </Routes>
+                
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/TriviaChannel/TriviaChannel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TriviaChannel = () => {
+    return (
+        <div>
+            <h1>Trivia Channel</h1>
+            <iframe src="http://localhost:5000" title="Trivia App" width="100%" height="600px"></iframe>
+        </div>
+    );
+}
+
+export default TriviaChannel;
